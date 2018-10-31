7685cde04fa33252824ef18b047eb3318f24f18a
leobene <leobene@192.168.0.105>
2018-10-31 10:11:59 -0200

    Added end points for getting and posting an competition

--- app.py
+++ app.py
@@ -4,10 +4,24 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+competitions = [{
+  'competicao': '100m',
+  'atleta': 'Joao das Neves',
+  'value': 10.234,
+  'unidade': 's'
+}]
+
 class Competition(Resource):
     def get(self, name):
-        return{'competition': name}
+        for competition in competitions:
+            if competition['competicao'] == name:
+               return competition
+
+    def post(self, name):
+        competition = {'competicao': name, 'atleta':"Bolt", 'value': 12.00, 'unidade': "s" }
+        competitions.append(competition)
+        return competition
 
 api.add_resource(Competition, '/competition/<string:name>')
 
-app.run(port=5004)
+app.run(port=5006)
