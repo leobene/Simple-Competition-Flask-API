eb6d1099a9efee46d759490f8bef17030a8a3ca6
leobene <leobene@192.168.0.105>
2018-10-31 07:44:54 -0200

    Added a Flask-RESTful API

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
-		
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
-		
-@app.route('/')
-def index():
-	return 'Hello, Flask!'
-	
-app.run(port=5000)	
-#if __name__ == '__main__':
-	#app.run(debug=True)
+api = Api(app)
+
+class Competition(Resource):
+    def get(self, name):
+        return{'competition': name}
+
+api.add_resource(Competition, '/competition/<string:name>')
+
+app.run(port=5004)
