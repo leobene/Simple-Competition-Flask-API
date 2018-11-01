9d5168857a59faccfc7f73f6ed6a6ac2228ccfbb
leobene <leobene@192.168.0.105>
2018-11-01 05:56:05 -0200

    Added endpoint to finish a competition

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ class Entry(Resource):
 
 class EntryList(Resource):
     def get(self):
-      return {'competicoes': entrys}
+      return {'entradas': entrys}
 
 class Competition(Resource):
     def get(self, name):
@@ -52,9 +52,25 @@ class CompetitionList(Resource):
     def get(self):
       return {'competicoes': competitions}
 
+class Finish(Resource):
+    def get(self, name):
+        for competition in competitions:
+            if competition['competicao'] == name:
+               return competition['isFinished']
+        return{'competicao': None}, 404
+
+    def post(self, name):
+        for competition in competitions:
+            if competition['competicao'] == name:
+               comp = {'competicao': competition['competicao'], 'ranking':competition['ranking'], 'isFinished': True, 'numTrys': competition['numTrys'] }
+               competition.update(comp)
+               return competition, 201
+        return{'competicao': None}, 404
+
 api.add_resource(Entry, '/entry/<string:name>')
 api.add_resource(EntryList, '/entrys')
 api.add_resource(Competition, '/competition/<string:name>')
 api.add_resource(CompetitionList, '/competitions')
+api.add_resource(Finish, '/finish/<string:name>')
 
 app.run(port=5006, debug=True)
