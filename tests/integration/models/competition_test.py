a046868be5990e49dcfe95e3fa878c416363111e
leobene <leobene@192.168.0.101>
2018-11-21 02:17:35 -0200

    Added Integration tests

--- tests/integration/models/competition_test.py
+++ tests/integration/models/competition_test.py
@@ -5,8 +5,27 @@ from tests.base_test import BaseTest
 
 class CompetitionTest(BaseTest):
     def test_crud(self):
-        pass
+        with self.app_context():
+            competition = CompetitionModel('test', False, 1)
+
+            self.assertIsNone(CompetitionModel.find_by_name('test'), "Found an competition with name 'test' before save_to_db")
+
+            competition.save_to_db()
+
+            self.assertIsNotNone(CompetitionModel.find_by_name('test'),
+                                 "Did not find a competition with name 'test' after save_to_db")
+
+            competition.delete_from_db()
+
+            self.assertIsNone(CompetitionModel.find_by_name('test'), "Found an competition with name 'test' after delete_from_db")
 
     def test_competition_relationship(self):
         with self.app_context():
-            pass
+            competition = CompetitionModel('test', False, 1)
+            entry = EntryModel('test_entry', 19.99, 'm', 1)
+
+            competition.save_to_db()
+            entry.save_to_db()
+
+            self.assertIsNotNone(competition.entrys[0])
+            self.assertEqual(competition.entrys[0].atleta, 'test_entry')
--- tests/integration/models/entry_test.py
+++ tests/integration/models/entry_test.py
@@ -5,7 +5,33 @@ from tests.base_test import BaseTest
 
 class EntryTest(BaseTest):
     def test_crud(self):
-        pass
+        with self.app_context():
+            competition = CompetitionModel('test', False, 1)
+            competition.save_to_db()
+            entry = EntryModel('test', 19.99, 'm', 1)
+
+            self.assertIsNone(EntryModel.find_by_name('test'), "Found an entry with name 'test' before save_to_db")
+
+            self.assertEqual(EntryModel.find_athlete_tries(1, 'test'), 0, "The number of tries for the athelte 'test' before save_to_db was not 0")
+
+
+            entry.save_to_db()
+
+            self.assertIsNotNone(EntryModel.find_by_name('test'),
+                                 "Did not find an entry with name 'test' after save_to_db")
+
+            self.assertEqual(EntryModel.find_athlete_tries(1, 'test'), 1, "The number of tries for the athelte 'test' after save_to_db was not 1")
+
+            entry.delete_from_db()
+
+            self.assertIsNone(EntryModel.find_by_name('test'), "Found an entry with name 'test' after delete_from_db")
 
     def test_competition_relationship(self):
-        pass
+        with self.app_context():
+            competition = CompetitionModel('test_competition', False, 1)
+            entry = EntryModel('test', 19.99, 'm', 1)
+
+            competition.save_to_db()
+            entry.save_to_db()
+
+            self.assertEqual(entry.competition.competicao, 'test_competition')
